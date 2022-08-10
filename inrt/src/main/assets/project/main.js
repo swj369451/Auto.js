dfe994d2fa16a3fa62c8ddb2f482238f5899066a
swj369451 <swj369451@163.com>
2022-08-10 10:58:18 +0800

    蓝牙按钮配对

--- inrt/src/main/assets/project/main.js
+++ inrt/src/main/assets/project/main.js
@@ -1,9 +1,12 @@
 threads.start(function () {
-while(true){
- var beginBtn;
-    if (beginBtn = classNameContains("Button").textContains("立即开始").findOne(2000)) {
-        beginBtn.click();
+    while(true){
+        var beginBtn;
+        if (beginBtn = classNameContains("Button").textContains("立即开始").findOne(2000)) {
+            beginBtn.click();
+        }
+        var bluetoothBtn;
+        if (bluetoothBtn = classNameContains("Button").textContains("配对").findOne(2000)) {
+            bluetoothBtn.click();
+        }
     }
-}
-
 });
